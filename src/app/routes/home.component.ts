30077a6603cfac63975cdb998dac1afb6bd43708
Salathiel <salathiel@genese.name>
2025-01-25 08:58:46 +0100

    feat: detect language or redirect to a language-aware URI
    Signed-off-by: Salathiel <salathiel@genese.name>

--- src/app/routes/home.component.ts
+++ src/app/routes/home.component.ts
@@ -1,10 +1,42 @@
-import {Component} from '@angular/core';
+import {Component, DestroyRef, OnInit} from '@angular/core';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: '[appHomePage]',
   template: `
-    Home...
+    <nav>
+      <a routerLink="/">None</a>
+      -
+      <a routerLink="/en">English</a>
+      -
+      <a routerLink="/fr">Francais</a>
+    </nav>
+    Home... '{{ lang }}'
   `,
+  imports: [
+    RouterLink
+  ]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  protected lang?: string;
+
+  constructor(private readonly router: Router,
+              private readonly destroyRef: DestroyRef,
+              private readonly activatedRoute: ActivatedRoute,) {
+  }
+
+  ngOnInit() {
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+    const subscription = this.activatedRoute.params.subscribe(({language}) => {
+      if (language) this.lang = language;
+      else void this.router.navigate([this.#language], {
+        relativeTo: null,
+      })
+    });
+  }
+
+  get #language() {
+    // TODO: resolve language
+    return 'en';
+  }
 }
