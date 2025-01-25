e2c4f985d482d59363cdd997921e3b5401adc209
Salathiel <salathiel@genese.name>
2025-01-25 07:50:57 +0100

    feat: reset app content from default
    Signed-off-by: Salathiel <salathiel@genese.name>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  template: `
+    <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent {
-  title = 'code.photo';
 }
