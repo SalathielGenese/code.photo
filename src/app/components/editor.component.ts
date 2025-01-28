7d072e03d4951017d7e3a1b5c58e48762577f48b
Salathiel <salathiel@genese.name>
2025-01-28 03:30:59 +0100

    feat: preserve cursor position through consecutive edit/highlight cycles (which changes the DOM)
    Signed-off-by: Salathiel <salathiel@genese.name>

--- src/app/components/editor.component.ts
+++ src/app/components/editor.component.ts
@@ -1,20 +1,100 @@
-import {Component, effect, ElementRef, input, viewChild} from '@angular/core';
+import {Component, computed, ElementRef, Inject, input, PLATFORM_ID, viewChild} from '@angular/core';
 import {Settings} from '../domains/settings.domain';
-import {JsonPipe} from '@angular/common';
+import {isPlatformBrowser, JsonPipe, NgClass} from '@angular/common';
+import Prism from "prismjs";
+import "prismjs/plugins/autoloader/prism-autoloader";
 
 @Component({
   standalone: true,
   selector: '[appEditor]',
   imports: [
-    JsonPipe
+    JsonPipe,
+    NgClass
   ],
   template: `
     <pre>{{ settings() | json }}</pre>
-    <pre><code contenteditable="true" #code></code></pre>
+    <pre [ngClass]="classes()"><code (input)="editing=true; highlight()"
+                                     contenteditable="true"
+                                     #code></code></pre>
   `
 })
 export class EditorComponent {
   readonly settings = input<Settings>();
 
+  protected editing = false;
+  protected readonly classes = computed(() => [
+    `language-${this.settings()?.language}`
+  ]);
   protected readonly codeViewChild = viewChild<ElementRef<HTMLElement>>('code');
+
+  readonly #plugin = '/prismjs/plugins/%/prism-%.min';
+
+  constructor(@Inject(PLATFORM_ID) private readonly platformId: Object) {
+  }
+
+  protected highlight() {
+    if (isPlatformBrowser(this.platformId)) {
+      const target = this.codeViewChild()?.nativeElement!;
+      const selection = this.editing
+        ? this.#getSelection(target.parentElement!)
+        : undefined;
+      target.replaceChildren(target.textContent!);
+      Prism.highlightAll();
+      selection && this.#setSelection(target.parentElement!, selection);
+    }
+    this.editing = false;
+  }
+
+  #setSelection(parent: HTMLElement, {start = 0, end = 0}) {
+    // Find the position in the DOM tree
+    const {node: startNode, offset: startOffset} = this.#findNodeAndOffset(parent, start);
+    const {node: endNode, offset: endOffset} = this.#findNodeAndOffset(parent, end);
+    const range = document.createRange();
+
+    if (startNode && endNode) {
+      range.setStart(startNode, startOffset);
+      range.setEnd(endNode, endOffset);
+      const selection = getSelection()!;
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  }
+
+  #getSelection(parent: HTMLElement): Record<'start' | 'end', number> | void {
+    const selection = getSelection();
+    if (selection?.rangeCount) {
+      const range = selection.getRangeAt(0);
+      return {
+        end: this.#getTextOffset(parent, range.endContainer, range.endOffset),
+        start: this.#getTextOffset(parent, range.startContainer, range.startOffset),
+      };
+    }
+  }
+
+  #getTextOffset(root: Node, target: Node, offset: number) {
+    let WALKER = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, null), total = 0, isLast = false;
+    while (root = WALKER.nextNode()!) {
+      total += ((isLast = root === target) ? offset : (root as Text).length);
+      if (isLast) break;
+    }
+    return total
+  }
+
+  #findNodeAndOffset(root: Node, targetOffset: number): { node?: Node, offset: number } {
+    const walker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, null);
+    let offset = 0, length: number;
+
+    while (root = walker.nextNode()!) {
+      ({length} = (root as Text));
+      if (offset + length >= targetOffset) {
+        return {
+          node: root,
+          offset: targetOffset - offset
+        };
+      }
+      offset += length;
+    }
+
+    return {offset: 0};
+  }
 }
