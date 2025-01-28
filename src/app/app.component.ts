f22797b79ecc1838b2fe6290950f956784a1641e
Salathiel <salathiel@genese.name>
2025-01-29 00:54:43 +0100

    feat: add SEO tags
    Signed-off-by: Salathiel <salathiel@genese.name>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, effect, Inject, PLATFORM_ID, untracked} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {Meta, Title} from '@angular/platform-browser';
+import {L10nService} from './services/l10n.service';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   standalone: true,
@@ -10,4 +13,38 @@ import {RouterOutlet} from '@angular/router';
   `,
 })
 export class AppComponent {
+  constructor(meta: Meta,
+              title: Title,
+              l10nService: L10nService,
+              @Inject(PLATFORM_ID) platformId: Object) {
+    meta.addTag({name: 'og:type', content: 'website'});
+    meta.addTag({name: 'og:card', content: 'summary'});
+    meta.addTag({name: 'twitter:site', content: '@SalathielGenese'});
+    ['og:image', 'twitter:image']
+      .forEach(name => meta.addTag({name, content: 'https://code.photo/favicon.ico'}));
+
+    effect(() => {
+      const cache = l10nService.cache();
+
+      title.setTitle(cache['meta.title']);
+      ['og:title', 'twitter:title']
+        .forEach(name => meta.addTag({name, content: cache['meta.title']}));
+      ['description', 'og:description', 'twitter:description']
+        .forEach(name => meta.addTag({name, content: cache['meta.description']}));
+      meta.addTag({name: 'og:url', content: `https://code.photo/${untracked(l10nService.language)}`});
+
+      if (isPlatformBrowser(platformId)) {
+        const script = Object.assign(document.head.querySelector('script[type$="/ld+json"]') ?? document.createElement('script'), {
+          text: JSON.stringify({
+            description: cache['meta.description'],
+            '@context': cache['meta.context'],
+            name: cache['meta.title'],
+            '@type': 'WebPage',
+          }),
+          type: 'application/ld+json',
+        });
+        if (!document.head.contains(script)) document.head.appendChild(script);
+      }
+    });
+  }
 }
